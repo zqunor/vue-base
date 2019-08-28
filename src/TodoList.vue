fa52fe714ef5e0cba0c55bff0283de374bf9f86e
zqunor <zqunor@gmail.com>
2019-08-28 14:23:24 +0800

    简单的todolist功能

---
+++ src/TodoList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <input v-model='content' type="text">
+    <button @click='submit'>提交</button>
+    <todo-item
+      v-for="(item, index) of todoList"
+      :key="index"
+      :item="item"
+      :index="index"
+      @delete="finish"
+      >
+    </todo-item>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TodoItem
+  },
+  data () {
+    return {
+      content: '',
+      todoList: [],
+    }
+  },
+  methods: {
+    submit: function () {
+      this.todoList.push(this.content)
+      this.content = ''
+    },
+    finish: function (index) {
+      this.todoList.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input type="checkbox" @click="checkout"/> {{item}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo-item',
+  props: {
+    item: String,
+    index: Number,
+  },
+  methods: {
+    checkout: function () {
+      this.$emit('delete', this.index)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
